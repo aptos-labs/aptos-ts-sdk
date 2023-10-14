0c9f75f0bb995a7df517217b003996766f57b4b6
maayan <maayansavir@gmail.com>
2023-10-13 21:03:02 -0700

    add fee payer example

--- examples/javascript/simple_sponsored_transaction.js
+++ examples/javascript/simple_sponsored_transaction.js
@@ -1,26 +1,26 @@
 /**
  * Example to submit a simple sponsored transaction where Alice transfers APT coin to Bob
- * with a sponser account to pay for the gas fee
+ * with a Sponser account to pay for the gas fee
  */
-import { Account, Aptos, U64 } from "aptos";
+const aptos = require("aptos");
 
 const ALICE_INITIAL_BALANCE = 100_000_000;
 const SPONSER_INITIAL_BALANCE = 100_000_000;
 const BOB_INITIAL_BALANCE = 0;
 const TRANSFER_AMOUNT = 10;
 
-const example = async () => {
+(async () => {
   console.log(
     "This example will create three accounts (Alice, Bob and Sponser), fund Alice and Sponser, transfer between Alice and Bob with Sponser to pay the gas fee.",
   );
 
   // Setup the client
-  const aptos = new Aptos();
+  const sdk = new aptos.Aptos();
 
   // Create three accounts
-  const alice = Account.generate();
-  const bob = Account.generate();
-  const sponser = Account.generate();
+  const alice = aptos.Account.generate();
+  const bob = aptos.Account.generate();
+  const sponser = aptos.Account.generate();
 
   // Variables to hold Alice and Sponser accounts address
   const aliceAddres = alice.accountAddress.toString();
@@ -33,12 +33,12 @@ const example = async () => {
   console.log(`Sponser's address is: ${sponserAddress}`);
 
   // Fund Alice and Sponser accounts
-  await aptos.fundAccount({ accountAddress: alice.accountAddress.toString(), amount: ALICE_INITIAL_BALANCE });
-  await aptos.fundAccount({ accountAddress: sponser.accountAddress.toString(), amount: SPONSER_INITIAL_BALANCE });
+  await sdk.fundAccount({ accountAddress: alice.accountAddress.toString(), amount: ALICE_INITIAL_BALANCE });
+  await sdk.fundAccount({ accountAddress: sponser.accountAddress.toString(), amount: SPONSER_INITIAL_BALANCE });
 
   // Show account balances
-  const aliceBalanceBefore = await aptos.getAccountCoinsData({ accountAddress: aliceAddres });
-  const sponserBalanceBefore = await aptos.getAccountCoinsData({ accountAddress: sponserAddress });
+  const aliceBalanceBefore = await sdk.getAccountCoinsData({ accountAddress: aliceAddres });
+  const sponserBalanceBefore = await sdk.getAccountCoinsData({ accountAddress: sponserAddress });
 
   console.log("\n=== Balances ===\n");
   console.log(`Alice's balance is: ${aliceBalanceBefore[0].amount}`);
@@ -51,34 +51,34 @@ const example = async () => {
   // Generate a fee payer (aka sponser) transaction
   // with Alice as the sender and Sponser as the fee payer
   console.log("\n=== Submitting Transaction ===\n");
-  const transaction = await aptos.generateTransaction({
+  const transaction = await sdk.generateTransaction({
     sender: aliceAddres,
     feePayerAddress: sponserAddress,
     data: {
       function: "0x1::aptos_account::transfer",
       type_arguments: [],
-      arguments: [bob.accountAddress, new U64(TRANSFER_AMOUNT)],
+      arguments: [bob.accountAddress, new aptos.U64(TRANSFER_AMOUNT)],
     },
   });
 
   // Alice and Sponser to sign the transaction (order matters)
-  const senderSignature = aptos.signTransaction({ signer: alice, transaction });
-  const sponserSignature = aptos.signTransaction({ signer: sponser, transaction });
+  const senderSignature = sdk.signTransaction({ signer: alice, transaction });
+  const sponserSignature = sdk.signTransaction({ signer: sponser, transaction });
 
   // Submit the transaction to chain
-  const committedTxn = await aptos.submitTransaction({
+  const committedTxn = await sdk.submitTransaction({
     transaction,
     senderAuthenticator: senderSignature,
     secondarySignerAuthenticators: { feePayerAuthenticator: sponserSignature },
   });
 
   console.log(`Submitted transaction: ${committedTxn.hash}`);
-  await aptos.waitForTransaction({ txnHash: committedTxn.hash });
-  await sleep(500);
+  await sdk.waitForTransaction({ txnHash: committedTxn.hash });
+
   console.log("\n=== Balances after transfer ===\n");
-  const aliceBalanceAfter = await aptos.getAccountCoinsData({ accountAddress: aliceAddres });
-  const bobBalanceAfter = await aptos.getAccountCoinsData({ accountAddress: bobAddress });
-  const sponserBalanceAfter = await aptos.getAccountCoinsData({ accountAddress: sponserAddress });
+  const aliceBalanceAfter = await sdk.getAccountCoinsData({ accountAddress: aliceAddres });
+  const bobBalanceAfter = await sdk.getAccountCoinsData({ accountAddress: bobAddress });
+  const sponserBalanceAfter = await sdk.getAccountCoinsData({ accountAddress: sponserAddress });
 
   // Bob should have the transfer amount
   if (bobBalanceAfter[0].amount !== TRANSFER_AMOUNT) throw new Error("Bob's balance after transfer is incorrect");
@@ -94,11 +94,4 @@ const example = async () => {
   console.log(`Alice's final balance is: ${ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT}`);
   console.log(`Bob's balance is: ${TRANSFER_AMOUNT}`);
   console.log(`Sponser's balance is: ${sponserBalanceAfter[0].amount}`);
-};
-const sleep = async (timeMs: number): Promise<null> => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeMs);
-  });
-};
-
-example();
+})();
