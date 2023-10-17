3a1129b7bcaa652b0c1f0b4a7498e483f25ce660
Greg Nazario <greg@gnazar.io>
2023-10-17 00:30:51 -0400

    [docs] Setup docs github pages (#77)
    * [docs] Cleanup docs generation script
    
    Docs generation script now uses correct paths, gitignore now allows
    committing docs.
    
    * [docs] Update README
    
    * [docs] Publish alpha version of documentation
    
    * [docs] Change README links to be absolute
    
    These links are used both on the README and on the typedoc docs, so
    they need to be absolute to work properly.

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -1,4 +1,4 @@
 # Generate docs by version for the SDK
 # References the Github repo directly
 # shellcheck disable=SC2154
-cross-var npx typedoc src/index.ts --out "docs/aptos-$npm_package_version" --cleanOutputDir --excludeInternal --gitRemote https://github.com/aptos-labs/aptos-ts-sdk.git --includeVersion
+cross-var npx typedoc src/index.ts --out "docs/@aptos-labs/ts-sdk-$npm_package_version" --cleanOutputDir --excludeInternal --includeVersion
