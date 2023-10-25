cfa3d5a6553cecdf23be7eb11250365a689e48f7
Greg Nazario <greg@gnazar.io>
2023-10-25 14:55:10 -0700

    [typeTag] Use new typetag parser everywhere, remove old one
    Also, support generic properly with the new type tag parser

--- src/internal/coin.ts
+++ src/internal/coin.ts
@@ -2,10 +2,10 @@ import { AptosConfig } from "../api/aptosConfig";
 import { U64 } from "../bcs/serializable/movePrimitives";
 import { Account, AccountAddress } from "../core";
 import { GenerateTransactionOptions, SingleSignerTransaction } from "../transactions/types";
-import { StructTag, TypeTagStruct } from "../transactions/typeTag/typeTag";
 import { HexInput, AnyNumber, MoveResourceType } from "../types";
 import { APTOS_COIN } from "../utils/const";
 import { generateTransaction } from "./transactionSubmission";
+import { parseTypeTag } from "../transactions/typeTag/parser";
 
 export async function transferCoinTransaction(args: {
   aptosConfig: AptosConfig;
@@ -22,7 +22,7 @@ export async function transferCoinTransaction(args: {
     sender: sender.accountAddress.toString(),
     data: {
       function: "0x1::aptos_account::transfer_coins",
-      typeArguments: [new TypeTagStruct(StructTag.fromString(coinStructType))],
+      typeArguments: [parseTypeTag(coinStructType)],
       functionArguments: [AccountAddress.fromHexInput(recipient), new U64(amount)],
     },
     options,
