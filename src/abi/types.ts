7ef9938639b72c8f6d2671055e64e10386d9a8b2
xbtmatt <90358481+xbtmatt@users.noreply.github.com>
2023-10-30 22:07:34 -0700

    Adding a types folder

--- src/abi/types.ts
+++ src/abi/types.ts
@@ -2,7 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { AccountAddress } from "../core";
-import { HexInput } from "../types";
+import { HexInput, MoveFunction } from "../types";
 
 export type OneOrNone<T> = [T] | [];
 export type AccountAddressInput = HexInput | AccountAddress;
+
+export type AbiFunctions = {
+    moduleAddress: AccountAddress;
+    moduleName: string;
+    publicEntryFunctions: Array<MoveFunction>;
+    privateEntryFunctions: Array<MoveFunction>;
+    viewFunctions: Array<MoveFunction>;
+  };
+  
