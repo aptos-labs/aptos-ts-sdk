3dc814ccbe24b9ee5bd643a46d2930d363466629
maayan <maayansavir@gmail.com>
2023-11-02 13:20:25 -0400

    fix tests

--- tests/unit/helper.ts
+++ tests/unit/helper.ts
@@ -18,17 +18,17 @@ export const Ed25519WalletTestObject = {
   address: "0x28b829b524d7c24aa7fd8916573c814df766dae542f724e1cf8914536232c346",
   mnemonic: "shoot island position soft burden budget tooth cruel issue economy destroy above",
   path: "m/44'/637'/0'/0'/0'",
-  privateKey: "0x1eec55afc2f72c4ab7b46c84d761739035ac420a2b6b22cef3411adaf91ce1f7",
-  publicKey:
-    "0x04913871f1d6cb7b867e8671cf63cf7b4c43819539fa0074ff933434bf20bab825b335535251f720fff72fd8b567e414af84aacf2f26ec804562081f2e0b0c9478",
+  privateKey: "0x5d996aa76b3212142792d9130796cd2e11e3c445a93118c08414df4f66bc60ec",
+  publicKey: "0xea526ba1710343d953461ff68641f1b7df5f23b9042ffa2d2a798d3adb3f3d6c",
 };
 
 export const secp256k1WalletTestObject = {
   address: "0x4b4aa8759fcef40ba49e999409eb73a98252f44f6612a4de2b23bad5c37b15a6",
   mnemonic: "shoot island position soft burden budget tooth cruel issue economy destroy above",
   path: "m/44'/637'/0'/0/0",
-  privateKey: "0x5d996aa76b3212142792d9130796cd2e11e3c445a93118c08414df4f66bc60ec",
-  publicKey: "0xea526ba1710343d953461ff68641f1b7df5f23b9042ffa2d2a798d3adb3f3d6c",
+  privateKey: "0x1eec55afc2f72c4ab7b46c84d761739035ac420a2b6b22cef3411adaf91ce1f7",
+  publicKey:
+    "0x04913871f1d6cb7b867e8671cf63cf7b4c43819539fa0074ff933434bf20bab825b335535251f720fff72fd8b567e414af84aacf2f26ec804562081f2e0b0c9478",
 };
 
 export const ed25519 = {
