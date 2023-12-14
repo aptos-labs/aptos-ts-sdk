11b7443501a6ec54fed8e2515814357e20417944
avirajkhare00 <avirajkhare00@gmail.com>
2023-12-15 00:32:28 +0530

    [Aviraj] Use spaces instead of tabs

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,29 @@
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	moduleNameMapper: {
-		"^(\\.{1,2}/.*)\\.js$": "$1",
-	},
-	testEnvironment: "node",
-	coveragePathIgnorePatterns: [
-		"./src/internal/queries/",
-		"./src/types/generated",
-		"./tests/e2e/ans/publishANSContracts.ts",
-	],
-	testPathIgnorePatterns: ["dist/*", "examples/*"],
-	collectCoverage: true,
-	setupFiles: ["dotenv/config"],
-	coverageThreshold: {
-		global: {
-			branches: 50, // 90,
-			functions: 50, // 95,
-			lines: 50, // 95,
-			statements: 50, // 95,
-		},
-	},
-	// To help avoid exhausting all the available fds.
-	maxWorkers: 4,
-	// to check if pg and aptos local node is running
-	testEnvironment: 'node',
-	globalSetup: './pre-test.js',
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: [
+    "./src/internal/queries/",
+    "./src/types/generated",
+    "./tests/e2e/ans/publishANSContracts.ts",
+  ],
+  testPathIgnorePatterns: ["dist/*", "examples/*"],
+  collectCoverage: true,
+  setupFiles: ["dotenv/config"],
+  coverageThreshold: {
+    global: {
+      branches: 50, // 90,
+      functions: 50, // 95,
+      lines: 50, // 95,
+      statements: 50, // 95,
+    },
+  },
+  // To help avoid exhausting all the available fds.
+  maxWorkers: 4,
+  // to check if pg and aptos local node is running
+  testEnvironment: 'node',
+  globalSetup: './pre-test.js',
 };
--- pre-test.js
+++ pre-test.js
@@ -1,29 +1,29 @@
 // import { http } from 'node:http'
 
 module.exports = async () => {
-	const http = require('node:http')
+  const http = require('node:http')
 
-	let a, b, c
+  let a, b, c
 
-	http.get('http://127.0.0.1:8080/', (response) => {
-		a = response.statusCode
-	})
+  http.get('http://127.0.0.1:8080/', (response) => {
+    a = response.statusCode
+  })
 
-	http.get('http://127.0.0.1:8090/', (response) => {
-		b = response.statusCode
-	})
+  http.get('http://127.0.0.1:8090/', (response) => {
+    b = response.statusCode
+  })
 
-	http.get('http://127.0.0.1:8081/', (response) => {
-		c = response.statusCode
-	})
+  http.get('http://127.0.0.1:8081/', (response) => {
+    c = response.statusCode
+  })
 
-	if (a === 200) {
-		throw new Error("Issue in aptos local testnet")
-	}
-	if (b === 200) {
-		throw new Error("Issue in aptos local testnet")
-	}
-	if (c === 302) {
-		throw new Error("Issue in aptos local testnet")
-	}
+  if (a === 200) {
+    throw new Error("Issue in aptos local testnet")
+  }
+  if (b === 200) {
+    throw new Error("Issue in aptos local testnet")
+  }
+  if (c === 302) {
+    throw new Error("Issue in aptos local testnet")
+  }
 };
