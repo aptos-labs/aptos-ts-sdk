f104c54905ab6584c7b71f3f40d6aeda8f6e04e7
Greg Nazario <greg@gnazar.io>
2024-01-03 09:57:47 -0800

    [consts] Make upper bound numbers hardcoded
    Doing powers of bigints doesn't work on older versions of node,
    and it seems to be hard to reproduce, but still occurs.  Hardcoding
    these should have the same behavior, without these quirks.
    
    Fixes https://github.com/aptos-labs/aptos-wallet-adapter/issues/223

--- src/bcs/consts.ts
+++ src/bcs/consts.ts
@@ -4,9 +4,11 @@
 import { Uint8, Uint16, Uint32, Uint64, Uint128, Uint256 } from "../types";
 
 // Upper bound values for uint8, uint16, uint64 and uint128
-export const MAX_U8_NUMBER: Uint8 = 2 ** 8 - 1;
-export const MAX_U16_NUMBER: Uint16 = 2 ** 16 - 1;
-export const MAX_U32_NUMBER: Uint32 = 2 ** 32 - 1;
-export const MAX_U64_BIG_INT: Uint64 = BigInt(2) ** BigInt(64) - BigInt(1);
-export const MAX_U128_BIG_INT: Uint128 = BigInt(2) ** BigInt(128) - BigInt(1);
-export const MAX_U256_BIG_INT: Uint256 = BigInt(2) ** BigInt(256) - BigInt(1);
+export const MAX_U8_NUMBER: Uint8 = 255;
+export const MAX_U16_NUMBER: Uint16 = 65535;
+export const MAX_U32_NUMBER: Uint32 = 4294967295;
+export const MAX_U64_BIG_INT: Uint64 = BigInt("18446744073709551615");
+export const MAX_U128_BIG_INT: Uint128 = BigInt("340282366920938463463374607431768211455");
+export const MAX_U256_BIG_INT: Uint256 = BigInt(
+  "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+);
