eeb4a14dd15de8f2f50810861795bbf95a12a65d
jill <121921928+jillxuu@users.noreply.github.com>
2024-03-19 15:02:07 -0700

    [TMP] update asset uploader example to make it actually working (#327)
    * add asset uploader readme
    
    * address comments

--- examples/typescript/asset_uploader.ts
+++ examples/typescript/asset_uploader.ts
@@ -1,7 +1,35 @@
 /* eslint-disable no-console */
 import { Account, AptosConfig, Network, Aptos, AssetUploader } from "@aptos-labs/ts-sdk";
+import * as fs from "fs";
+import * as path from "path";
 
 const example = async () => {
+  const updateImageField = (jsonFilePath: string, imageUrl: string) => {
+    const metadataContent = fs.readFileSync(jsonFilePath, "utf8");
+    const metadataJson = JSON.parse(metadataContent);
+    metadataJson.image = imageUrl;
+    fs.writeFileSync(jsonFilePath, JSON.stringify(metadataJson, null, 4));
+    return metadataJson;
+  };
+
+  // Function to recursively update the image fields in all JSON files in a folder synchronously
+  const updateImageFieldsInFolder = (folderPath: string, mediaFolderUrl: string) => {
+    const files = fs.readdirSync(folderPath);
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+      if (filePath.endsWith(".json")) {
+        // Assuming the corresponding image has the same base name as the JSON file
+        const baseName = path.basename(file, ".json");
+        // Find the corresponding image file
+        const imageFile = files.find((f) => path.basename(f, path.extname(f)) === baseName);
+        if (imageFile) {
+          const imageUrl = `${mediaFolderUrl}/${imageFile}`;
+          updateImageField(filePath, imageUrl);
+        }
+      }
+    });
+  };
+
   const account = Account.generate();
   const aptosConfig = new AptosConfig({
     network: Network.TESTNET,
@@ -13,27 +41,56 @@ const example = async () => {
   await aptos.fundAccount({ accountAddress: account.accountAddress, amount: 1_000_000 });
 
   // Fund a node
-  const fundResponse = await assetUploader.fundNode({ account, amount: 1000 });
+  const fundResponse = await assetUploader.fundNode({ account, amount: 1_000_000 });
   console.log("fund response", fundResponse);
 
-  // Upload data
-  const uploadDataResponse = await assetUploader.uploadData({ account, data: "Hello Aptos" });
-  console.log("upload response", uploadDataResponse);
-
-  // Upload file
+  // Upload collection media
+  const collectionMediaPath = "/collection_media_file_path"; // Update this path
   const uploadFileResponse = await assetUploader.uploadFile({
     account,
-    file: "/Users/maayansavir/Desktop/pfp.jpeg",
+    file: collectionMediaPath,
   });
-  console.log("uploadFileResponse", uploadFileResponse);
+  const collectionMediaManifestID = uploadFileResponse.id;
+  const collectionMediaURI = `https://arweave.net/${collectionMediaManifestID}`;
+  console.log("Collection Media URL:", collectionMediaURI);
 
-  // Upload folder
+  // Update collection metadata JSON file
+  const collectionMetadataJsonPath = "/collection_metadata_json_file_path"; // Update this path
+  updateImageField(collectionMetadataJsonPath, collectionMediaURI);
+
+  // Upload updated collection metadata json
+  const uploadJsonResponse = await assetUploader.uploadFile({
+    account,
+    file: collectionMetadataJsonPath,
+  });
+  const collectionMetadataJsonManifestID = uploadJsonResponse.id;
+  const collectionMetadataJsonURI = `https://arweave.net/${collectionMetadataJsonManifestID}`;
+  console.log("Collection metadata json URL:", collectionMetadataJsonURI);
+
+  // Upload token media folder
+  const tokenMediaFolderPath = "/token_media_folder_path"; // Update this path
   const uploadFolderResponse = await assetUploader.uploadFolder({
     account,
-    folder: "/Users/maayansavir/Desktop/test-apps/web-app",
+    folder: tokenMediaFolderPath,
   });
-  console.log("uploadFolderResponse", uploadFolderResponse);
-  process.exit(0);
+  const tokenMediaFolderManifestID = uploadFolderResponse!.id;
+  const tokenMediaFolderURI = `https://arweave.net/${tokenMediaFolderManifestID}`;
+  console.log("Token media URL folder:", tokenMediaFolderURI);
+
+  // Update token metadata JSON files with token media URL
+  const tokenMetadataJsonFolderPath = "/token_metadata_json_folder_path"; // Update this path
+  updateImageFieldsInFolder(tokenMetadataJsonFolderPath, tokenMediaFolderURI);
+
+  // Upload the updated token metadata json folder
+  const uploadMetadataFolderResponse = await assetUploader.uploadFolder({
+    account,
+    folder: tokenMetadataJsonFolderPath,
+  });
+  const tokenMetadataJsonManifestID = uploadMetadataFolderResponse!.id;
+  const tokenMetadataJsonFolderURI = `https://arweave.net/${tokenMetadataJsonManifestID}`;
+  console.log("Token metadata json URL folder:", tokenMetadataJsonFolderURI);
+
+  // DONE
 };
 
 example();
