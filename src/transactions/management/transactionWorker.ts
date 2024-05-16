7a78d482c7b07d7fbb5add4d1726d5461ab21b9d
Oliver <heliuchuan@gmail.com>
2024-05-16 19:09:29 -0400

    remove waitfortxn changes

--- src/transactions/management/transactionWorker.ts
+++ src/transactions/management/transactionWorker.ts
@@ -233,13 +233,7 @@ export class TransactionWorker extends EventEmitter<TransactionWorkerEvents> {
   async checkTransaction(sentTransaction: PromiseFulfilledResult<PendingTransactionResponse>, sequenceNumber: bigint) {
     try {
       const waitFor: Array<Promise<TransactionResponse>> = [];
-      waitFor.push(
-        waitForTransaction({
-          aptosConfig: this.aptosConfig,
-          transactionHash: sentTransaction.value.hash,
-          options: { timeoutSecs: 3 },
-        }),
-      );
+      waitFor.push(waitForTransaction({ aptosConfig: this.aptosConfig, transactionHash: sentTransaction.value.hash }));
       const sentTransactions = await Promise.allSettled(waitFor);
 
       for (let i = 0; i < sentTransactions.length; i += 1) {
