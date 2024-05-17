92ff8f78220d3edf13c6b7e45b9c01a17ab41c9b
Oliver <heliuchuan@gmail.com>
2024-05-16 21:04:22 -0400

    add poseidon test

---
+++ tests/unit/poseidon.test.ts
@@ -0,0 +1,15 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+import { poseidonHash } from "../../src/core/crypto/poseidon";
+
+describe("Poseidon", () => {
+  it("should hash correctly", () => {
+    const input = [1,2]
+    let hash = poseidonHash(input);
+    expect(hash).toEqual(BigInt("7853200120776062878684798364095072458815029376092732009249414926327459813530"));
+    input.pop()
+    hash = poseidonHash(input);
+    expect(hash).toEqual(BigInt("18586133768512220936620570745912940619677854269274689475585506675881198879027"));
+  });
+});
