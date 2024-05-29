592c2228074e9c797adb229971bb60f582118e1f
Oliver <heliuchuan@gmail.com>
2024-05-29 14:07:03 -0400

    add poseidon test

--- tests/unit/poseidon.test.ts
+++ tests/unit/poseidon.test.ts
@@ -1,7 +1,7 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-import { poseidonHash } from "../../src/core/crypto/poseidon";
+import { bigIntToBytesLE, bytesToBigIntLE, hashStrToField, poseidonHash } from "../../src/core/crypto/poseidon";
 
 describe("Poseidon", () => {
   it("should hash correctly", () => {
@@ -12,6 +12,22 @@ describe("Poseidon", () => {
     ];
     for (let i = 0; i < input.length; i += 1) {
       expect(poseidonHash(input[i])).toEqual(expected[i]);
+    }
+  });
+  it("should hash strings correctly", () => {
+    const input = ["hello", "google"];
+    const expected = [
+      BigInt("19131502131677697582824316262023599653223229634739282128274922348992854400539"),
+      BigInt("10420754430899002178577798392930147671924237248238291825718956074978332229675"),
+    ];
+    for (let i = 0; i < input.length; i += 1) {
+      expect(hashStrToField(input[i], 31)).toEqual(expected[i]);
+    }
+  });
+  it("should convert bigint to array and back correctly", () => {
+    const input = [BigInt(123), BigInt(321)];
+    for (let i = 0; i < input.length; i += 1) {
+      expect(input[i]).toEqual( bytesToBigIntLE(bigIntToBytesLE(input[i], 31)));
     }
   });
 });
