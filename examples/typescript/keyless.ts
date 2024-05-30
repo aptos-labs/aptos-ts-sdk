db25714e130400c89e8daec38eabcafc15fdb8e7
Oliver <heliuchuan@gmail.com>
2024-05-30 06:08:35 -0400

    whitespace

--- examples/typescript/keyless.ts
+++ examples/typescript/keyless.ts
@@ -99,7 +99,7 @@ const example = async () => {
     amount: TRANSFER_AMOUNT,
   });
 
-    const committedTxn = await aptos.signAndSubmitTransaction({ signer: alice, transaction });
+  const committedTxn = await aptos.signAndSubmitTransaction({ signer: alice, transaction });
 
   await aptos.waitForTransaction({ transactionHash: committedTxn.hash });
   console.log(`Committed transaction: ${committedTxn.hash}`);
