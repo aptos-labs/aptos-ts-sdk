af8e4b518f407359e216af2022d9d6c8c74e1637
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2024-07-02 12:30:04 -0700

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -568,7 +568,7 @@ export type BlockEndInfo = {
   block_output_limit_reached: boolean;
   block_effective_block_gas_units: number;
   block_approx_output_size: number;
-}
+};
 
 export type BlockEpilogueTransactionResponse = {
   type: TransactionResponseType.BlockEpilogue;
