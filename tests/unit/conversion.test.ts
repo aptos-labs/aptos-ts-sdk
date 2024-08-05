95f00f90d8f7df3c59e2a0964891eb36832b8d3a
maayan <maayansavir@gmail.com>
2024-08-05 12:27:39 -0400

    add float number tests

--- tests/unit/conversion.test.ts
+++ tests/unit/conversion.test.ts
@@ -8,10 +8,38 @@ describe("conversion", () => {
     expect(result).toBe(50000000000);
   });
 
+  test("it converts float amount from human readable format to on chain format", () => {
+    const value = 500.5;
+    const decimals = 8;
+    const result = convertAmountFromHumanReadableToOnChain(value, decimals);
+    expect(result).toBe(50050000000);
+  });
+
+  test("it converts float amount from human readable format to on chain format", () => {
+    const value = 500.598;
+    const decimals = 8;
+    const result = convertAmountFromHumanReadableToOnChain(value, decimals);
+    expect(result).toBe(50059800000);
+  });
+
   test("it converts amount from on chain format to human readable format ", () => {
     const value = 50000000000;
     const decimals = 8;
     const result = convertAmountFromOnChainToHumanReadable(value, decimals);
     expect(result).toBe(500);
+  });
+
+  test("it converts float amount from on chain format to human readable format ", () => {
+    const value = 50050000000;
+    const decimals = 8;
+    const result = convertAmountFromOnChainToHumanReadable(value, decimals);
+    expect(result).toBe(500.5);
+  });
+
+  test("it converts float amount from on chain format to human readable format ", () => {
+    const value = 50059800000;
+    const decimals = 8;
+    const result = convertAmountFromOnChainToHumanReadable(value, decimals);
+    expect(result).toBe(500.598);
   });
 });
