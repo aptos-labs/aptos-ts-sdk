d6c4bd0b8f74f2d38dea332443c2d9e5300eaa21
Oliver <heliuchuan@gmail.com>
2024-09-10 19:34:53 -0400

    revert endpoints

--- src/utils/apiEndpoints.ts
+++ src/utils/apiEndpoints.ts
@@ -25,17 +25,17 @@ export const NetworkToFaucetAPI: Record<string, string> = {
 export const NetworkToPepperAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/keyless/pepper/v0",
   testnet: "https://api.testnet.aptoslabs.com/keyless/pepper/v0",
-  devnet: "https://pepper.keyless.devnet.aptoslabs.com/v0",
+  devnet: "https://api.devnet.aptoslabs.com/keyless/pepper/v0",
   // Use the devnet service for local environment
-  local: "https://pepper.keyless.devnet.aptoslabs.com/v0",
+  local: "https://api.devnet.aptoslabs.com/keyless/pepper/v0",
 };
 
 export const NetworkToProverAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/keyless/prover/v0",
   testnet: "https://api.testnet.aptoslabs.com/keyless/prover/v0",
-  devnet: "https://prover.keyless.devnet.aptoslabs.com/v0",
+  devnet: "https://api.devnet.aptoslabs.com/keyless/prover/v0",
   // Use the devnet service for local environment
-  local: "https://prover.keyless.devnet.aptoslabs.com/v0",
+  local: "https://api.devnet.aptoslabs.com/keyless/prover/v0",
 };
 
 export enum Network {
