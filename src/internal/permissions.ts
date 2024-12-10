cb712f9534769310544ea2bfa25ed5c5c9a8aeff
max <dumblole@gmail.com>
2024-12-10 11:09:07 -0800

    Permissioned Accounts (#594)
    * Adds stubs for permission work
    
    * someone fucking cooked here
    
    * works
    
    * all the tests pass
    
    * organize
    
    * respond to some comments
    
    * use parsed permission
    
    * fix test file
    
    * fix
    
    * all tests pass
    
    ---------
    
    Co-authored-by: Rasa Welcher <kwelch12@gmail.com>

---
+++ src/internal/permissions.ts
@@ -0,0 +1,329 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * This file contains the underlying implementations for exposed API surface in
+ * the {@link api/name}. By moving the methods out into a separate file,
+ * other namespaces and processes can access these methods without depending on the entire
+ * name namespace and without having a dependency cycle error.
+ */
+
+import { BatchArgument } from "@wgb5445/aptos-intent-npm";
+import { AptosConfig } from "../api/aptosConfig";
+import { AccountAddress, Ed25519PublicKey } from "../core";
+import { SimpleTransaction } from "../transactions/instances/simpleTransaction";
+import { MoveString } from "../bcs";
+import { AptosIntentBuilder } from "../transactions";
+import {
+  FilteredPermissions,
+  PermissionType,
+  MoveVMPermissionType,
+  RevokePermission,
+  Permission,
+  buildFungibleAssetPermission,
+  buildNFTPermission,
+} from "../types/permissions";
+import { Transaction } from "../api/transaction";
+import { view } from "./view";
+
+// functions
+export async function getPermissions<T extends PermissionType>({
+  aptosConfig,
+  primaryAccountAddress,
+  subAccountPublicKey,
+  filter,
+}: {
+  aptosConfig: AptosConfig;
+  primaryAccountAddress: AccountAddress;
+  subAccountPublicKey: Ed25519PublicKey;
+  filter?: T;
+}): Promise<FilteredPermissions<T>> {
+  const handle = await getHandleAddress({ aptosConfig, primaryAccountAddress, subAccountPublicKey });
+
+  const res = await fetch(`${aptosConfig.fullnode}/accounts/${handle}/resources`);
+
+  type NodeDataResponse = Array<{
+    type: string;
+    data: {
+      perms: {
+        data: Array<{
+          key: { data: string; type_name: string };
+          value: string;
+        }>;
+      };
+    };
+  }>;
+
+  const data = (await res.json()) as NodeDataResponse;
+
+  const permissions = data[0].data.perms.data.map((d) => {
+    switch (d.key.type_name) {
+      case MoveVMPermissionType.FungibleAsset: // can this be better? i dont rly like this
+        return buildFungibleAssetPermission({
+          asset: d.key.data,
+          balance: d.value,
+        });
+      case MoveVMPermissionType.TransferPermission:
+        return buildNFTPermission({
+          assetAddress: d.key.data,
+          capabilities: { transfer: true, mutate: false },
+        });
+      default:
+        // todo throw here
+        throw new Error();
+    }
+  });
+
+  const filtered = filter ? permissions.filter((p) => filter.includes(p.type as PermissionType)) : permissions;
+  return filtered as FilteredPermissions<T>;
+}
+
+// should it return the requested permissions? on success? and when it fails it
+export async function requestPermission(args: {
+  aptosConfig: AptosConfig;
+  primaryAccountAddress: AccountAddress;
+  permissionedAccountPublicKey: Ed25519PublicKey;
+  permissions: Permission[];
+  expiration?: number;
+  requestsPerSecond?: number;
+}): Promise<SimpleTransaction> {
+  const { aptosConfig, primaryAccountAddress, permissionedAccountPublicKey, permissions } = args;
+  const transaction = new Transaction(aptosConfig);
+
+  // Get or create a handle for the permissioned account
+  const existingHandleAddress = await getHandleAddress({
+    aptosConfig,
+    primaryAccountAddress,
+    subAccountPublicKey: permissionedAccountPublicKey,
+  });
+
+  return transaction.build.batched_intents({
+    sender: primaryAccountAddress,
+    builder: async (builder) => {
+      // Get the permissioned signer - either create new one or use existing
+      const permissionedSigner = await getPermissionedSigner(builder, {
+        existingHandleAddress,
+        permissionedAccountPublicKey,
+      });
+
+      // if nft permission has multiple capabilities, we need to add multiple txns
+      // For NFT permissions with multiple capabilities, split into separate transactions
+
+      const expandedPermissions = permissions.flatMap((permission) => {
+        if (permission.type === PermissionType.NFT && permission.capabilities) {
+          const expanded = [];
+          if (permission.capabilities.transfer) {
+            expanded.push({
+              ...permission,
+              capabilities: { transfer: true, mutate: false },
+            });
+          }
+          if (permission.capabilities.mutate) {
+            expanded.push({
+              ...permission,
+              capabilities: { transfer: false, mutate: true },
+            });
+          }
+          return expanded;
+        }
+        return permission;
+      });
+      // Grant each requested permission
+      await Promise.all(
+        expandedPermissions
+          .map((permission) =>
+            grantPermission(builder, {
+              permissionedSigner: permissionedSigner.signer,
+              permission,
+            }),
+          )
+          .flat(),
+      );
+
+      // If we created a new handle, finalize the setup
+      if (permissionedSigner.isNewHandle) {
+        await finalizeNewHandle(builder, {
+          permissionedAccountPublicKey,
+          handle: permissionedSigner.handle!,
+        });
+      }
+
+      return builder;
+    },
+  });
+}
+
+export async function revokePermissions(args: {
+  aptosConfig: AptosConfig;
+  primaryAccountAddress: AccountAddress;
+  subAccountPublicKey: Ed25519PublicKey;
+  permissions: RevokePermission[];
+}): Promise<SimpleTransaction> {
+  const { aptosConfig, primaryAccountAddress, subAccountPublicKey, permissions } = args;
+
+  const transaction = new Transaction(aptosConfig);
+  return transaction.build.batched_intents({
+    sender: primaryAccountAddress,
+    builder: async (builder) => {
+      const signer = await builder.add_batched_calls({
+        function: "0x1::permissioned_delegation::permissioned_signer_by_key",
+        functionArguments: [BatchArgument.new_signer(0), subAccountPublicKey.toUint8Array()],
+        typeArguments: [],
+      });
+
+      const permissionPromises = permissions.map((permission) => {
+        switch (permission.type) {
+          case PermissionType.FungibleAsset: {
+            return builder.add_batched_calls({
+              function: "0x1::fungible_asset::revoke_permission",
+              functionArguments: [signer[0].borrow(), permission.asset],
+              typeArguments: [],
+            });
+          }
+          // TODO: object nft revoke
+          case PermissionType.NFT: {
+            return builder.add_batched_calls({
+              function: "0x1::object::revoke_permission",
+              functionArguments: [signer[0].borrow(), permission.assetAddress],
+              typeArguments: ["0x4::token::Token"],
+            });
+          }
+          default: {
+            console.log("Not implemented");
+            return Promise.resolve();
+          }
+        }
+      });
+
+      await Promise.all(permissionPromises);
+      return builder;
+    },
+  });
+}
+
+//  helper functions
+async function getPermissionedSigner(
+  builder: AptosIntentBuilder,
+  args: {
+    existingHandleAddress: string | null;
+    permissionedAccountPublicKey: Ed25519PublicKey;
+  },
+) {
+  if (args.existingHandleAddress) {
+    const signer = await builder.add_batched_calls({
+      function: "0x1::permissioned_delegation::permissioned_signer_by_key",
+      functionArguments: [BatchArgument.new_signer(0), args.permissionedAccountPublicKey.toUint8Array()],
+      typeArguments: [],
+    });
+    return { signer, isNewHandle: false };
+  }
+
+  // Create new handle and signer
+  const handle = await builder.add_batched_calls({
+    function: "0x1::permissioned_signer::create_storable_permissioned_handle",
+    functionArguments: [BatchArgument.new_signer(0), 360],
+    typeArguments: [],
+  });
+
+  const signer = await builder.add_batched_calls({
+    function: "0x1::permissioned_signer::signer_from_storable_permissioned",
+    functionArguments: [handle[0].borrow()],
+    typeArguments: [],
+  });
+
+  return { signer, handle, isNewHandle: true };
+}
+
+async function grantPermission(
+  builder: AptosIntentBuilder,
+  args: {
+    permissionedSigner: BatchArgument[];
+    permission: Permission;
+  },
+) {
+  switch (args.permission.type) {
+    case PermissionType.FungibleAsset:
+      return builder.add_batched_calls({
+        function: "0x1::fungible_asset::grant_permission",
+        functionArguments: [
+          BatchArgument.new_signer(0),
+          args.permissionedSigner[0].borrow(),
+          args.permission.asset, // do i need to convert this to AccountAddress? .... i guess not??
+          args.permission.balance,
+        ],
+        typeArguments: [],
+      });
+    case PermissionType.NFT: {
+      const txn: Promise<BatchArgument[]>[] = [];
+      if (args.permission.capabilities.transfer) {
+        return builder.add_batched_calls({
+          function: "0x1::object::grant_permission",
+          functionArguments: [
+            BatchArgument.new_signer(0),
+            args.permissionedSigner[0].borrow(),
+            args.permission.assetAddress,
+          ],
+          typeArguments: ["0x4::token::Token"],
+        });
+      }
+      if (args.permission.capabilities.mutate) {
+        console.log("mutate not implemented");
+        throw new Error("mutate not implemented");
+      }
+      return txn;
+    }
+    default:
+      console.log("Not implemented");
+      throw new Error(`${args.permission.type} not implemented`);
+      return Promise.resolve();
+  }
+}
+
+async function finalizeNewHandle(
+  builder: AptosIntentBuilder,
+  args: {
+    permissionedAccountPublicKey: Ed25519PublicKey;
+    handle: BatchArgument[];
+  },
+) {
+  await builder.add_batched_calls({
+    function: "0x1::permissioned_delegation::add_permissioned_handle",
+    functionArguments: [BatchArgument.new_signer(0), args.permissionedAccountPublicKey.toUint8Array(), args.handle[0]],
+    typeArguments: [],
+  });
+
+  await builder.add_batched_calls({
+    function: "0x1::lite_account::add_dispatchable_authentication_function",
+    functionArguments: [
+      BatchArgument.new_signer(0),
+      AccountAddress.ONE,
+      new MoveString("permissioned_delegation"),
+      new MoveString("authenticate"),
+    ],
+    typeArguments: [],
+  });
+}
+
+export async function getHandleAddress({
+  aptosConfig,
+  primaryAccountAddress,
+  subAccountPublicKey,
+}: {
+  aptosConfig: AptosConfig;
+  primaryAccountAddress: AccountAddress;
+  subAccountPublicKey: Ed25519PublicKey;
+}) {
+  try {
+    const [handle] = await view<string[]>({
+      aptosConfig,
+      payload: {
+        function: "0x1::permissioned_delegation::handle_address_by_key",
+        functionArguments: [primaryAccountAddress, subAccountPublicKey.toUint8Array()],
+      },
+    });
+
+    return handle;
+  } catch {
+    return null;
+  }
+}
