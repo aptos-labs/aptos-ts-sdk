cda1c441b275b3550eb25769c7b68cbfaef6db97
GhostWalker562 <pvu2002@outlook.com>
2025-02-06 11:44:34 -0800

    Remove any_authenticator example for complex public key delegation example

--- examples/typescript/public_key_authenticator_account_abstraction.ts
+++ examples/typescript/public_key_authenticator_account_abstraction.ts
@@ -7,7 +7,7 @@ import {
   Network,
   AptosConfig,
   UserTransactionResponse,
-  Hex,
+  Serializer,
 } from "@aptos-labs/ts-sdk";
 import { compilePackage, getPackageBytesToPublish } from "./utils";
 
@@ -15,23 +15,26 @@ const aptos = new Aptos(new AptosConfig({ network: Network.DEVNET }));
 
 const main = async () => {
   const alice = Account.generate();
+  const bob = Account.generate();
 
   console.log("\n=== Addresses ===");
   console.log(`Alice: ${alice.accountAddress.toString()}`);
-
+  console.log(`Bob: ${bob.accountAddress.toString()}`);
   console.log("\n=== Funding Accounts ===");
   await aptos.fundAccount({ accountAddress: alice.accountAddress, amount: 1000000000000000 });
   console.log("Finished funding accounts!");
 
-  console.log("\n=== Compiling any_authenticator package locally ===");
+  console.log("\n=== Compiling public_key_authenticator package locally ===");
   compilePackage(
     "move/account_abstraction",
-    "move/account_abstraction/any_authenticator.json",
+    "move/account_abstraction/public_key_authenticator.json",
     [{ name: "deployer", address: alice.accountAddress }],
     ["--move-2"],
   );
-  const { metadataBytes, byteCode } = getPackageBytesToPublish("move/account_abstraction/any_authenticator.json");
-  console.log(`\n=== Publishing any_authenticator package to ${aptos.config.network} network ===`);
+  const { metadataBytes, byteCode } = getPackageBytesToPublish(
+    "move/account_abstraction/public_key_authenticator.json",
+  );
+  console.log(`\n=== Publishing public_key_authenticator package to ${aptos.config.network} network ===`);
   const publishTxn = await aptos.publishPackageTransaction({
     account: alice.accountAddress,
     metadataBytes,
@@ -43,16 +46,14 @@ const main = async () => {
 
   console.log("\n=== Dispatchable authentication function info ===");
 
-  const authenticationFunction = `${alice.accountAddress}::any_authenticator::authenticate`;
+  const authenticationFunction = `${alice.accountAddress}::public_key_authenticator::authenticate`;
   const [moduleAddress, moduleName, functionName] = authenticationFunction.split("::");
 
   console.log(`Module address: ${moduleAddress}`);
   console.log(`Module name: ${moduleName}`);
   console.log(`Function name: ${functionName}`);
 
-  console.log(
-    `\n=== Changing ${alice.accountAddress.toString()} to use any_authenticator's AccountAbstraction function ===`,
-  );
+  console.log(`\n=== Enabling account abstraction for ${alice.accountAddress.toString()} ===`);
   const enableAccountAbstractionTransaction = await aptos.abstraction.enableAccountAbstractionTransaction({
     accountAddress: alice.accountAddress,
     authenticationFunction,
@@ -64,11 +65,32 @@ const main = async () => {
   console.log(`Enable account abstraction transaction hash: ${pendingEnableAccountAbstractionTransaction.hash}`);
   await aptos.waitForTransaction({ transactionHash: pendingEnableAccountAbstractionTransaction.hash });
 
-  console.log("\n=== Signing a transaction with the abstracted account ===");
+  console.log("\n=== Permitting Bob's public key to sign on behalf of Alice");
+  const enableBobPublicKeyTransaction = await aptos.transaction.build.simple({
+    sender: alice.accountAddress,
+    data: {
+      function: `${alice.accountAddress}::public_key_authenticator::permit_public_key`,
+      typeArguments: [],
+      functionArguments: [bob.publicKey.toUint8Array()],
+    },
+  });
+  const pendingEnableBobPublicKeyTransaction = await aptos.signAndSubmitTransaction({
+    signer: alice,
+    transaction: enableBobPublicKeyTransaction,
+  });
+  console.log(`Enable Bob's public key transaction hash: ${pendingEnableBobPublicKeyTransaction.hash}`);
+  await aptos.waitForTransaction({ transactionHash: pendingEnableBobPublicKeyTransaction.hash });
+
+  console.log("\n=== Signing a transaction with the abstracted account using Bob's signer ===");
 
   const abstractedAccount = new AbstractedAccount({
-    signer: () => Hex.fromHexString("0x01").toUint8Array(),
     accountAddress: alice.accountAddress,
+    signer: (digest) => {
+      const serializer = new Serializer();
+      bob.publicKey.serialize(serializer);
+      bob.sign(digest).serialize(serializer);
+      return serializer.toUint8Array();
+    },
     authenticationFunction,
   });
   const pendingTransferTxn = await aptos.signAndSubmitTransaction({
