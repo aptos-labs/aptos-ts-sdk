867e3e95bcc05a8c492df524127720d72ef31785
Oliver <heliuchuan@gmail.com>
2025-05-22 00:13:59 +0200

    update

--- confidential-assets/jest.config.js
+++ confidential-assets/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require("../jest.config.js"),
+  testPathIgnorePatterns: [],
   coveragePathIgnorePatterns: [],
   globalSetup: "../tests/preTest.cjs",
   globalTeardown: "../tests/postTest.cjs",
