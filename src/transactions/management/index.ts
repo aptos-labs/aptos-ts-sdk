cedra-labs/cedra-ts-sdk

New transaction worker proposal

src/transactions/management/index.ts
@@ -1,2 +1,3 @@
 export * from "./accountSequenceNumber";
 export * from "./transactionWorker";
+export * from "./transactionWorker2";

aptos-labs/aptos-ts-sdk

src/transactions/management/index.ts
@@ -1,2 +1,3 @@
 export * from "./accountSequenceNumber";
+export * from "./newTransactionWorker";
 export * from "./transactionWorker";