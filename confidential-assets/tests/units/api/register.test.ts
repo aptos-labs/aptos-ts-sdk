71b77813c5cbaf58ba74e904bdad95bf6916ba17
lukachi <lukachisama@gmail.com>
2025-06-27 18:42:58 +0300

    refactor: update deposit, register, safely rollover, and transfer tests to use signer directly
    - Removed unnecessary preload table map tests from deposit, register, safely rollover, and transfer test files.
    - Updated deposit and register functions to use `signer` instead of `sender` for better clarity and consistency.
    - Adjusted gas usage logging to directly reference the transaction object.
    - Ensured all tests maintain functionality while improving readability and maintainability.

--- confidential-assets/tests/units/api/register.test.ts
+++ confidential-assets/tests/units/api/register.test.ts
@@ -3,37 +3,27 @@ import {
   getTestAccount,
   getTestConfidentialAccount,
   longTestTimeout,
-  sendAndWaitTx,
   TOKEN_ADDRESS,
 } from "../../helpers";
-import { preloadTables } from "../../helpers/wasmPollardKangaroo";
 
 describe("Register", () => {
   const alice = getTestAccount();
   const aliceConfidential = getTestConfidentialAccount(alice);
 
-  it(
-    "Pre load wasm table map",
-    async () => {
-      await preloadTables();
-    },
-    longTestTimeout,
-  );
+  console.log(alice.accountAddress.toString())
 
   it(
     "it should register Alice confidential balance",
     async () => {
-      const aliceRegisterVBTxBody = await confidentialAsset.registerBalance({
-        sender: alice.accountAddress,
+      const tx = await confidentialAsset.registerBalance({
+        decryptionKey: aliceConfidential,
         tokenAddress: TOKEN_ADDRESS,
-        publicKey: aliceConfidential.publicKey(),
+        signer: alice,
       });
 
-      const aliceTxResp = await sendAndWaitTx(aliceRegisterVBTxBody, alice);
+      console.log("gas used:", tx.gas_used);
 
-      console.log("gas used:", aliceTxResp.gas_used);
-
-      expect(aliceTxResp.success).toBeTruthy();
+      expect(tx.success).toBeTruthy();
     },
     longTestTimeout,
   );
--- confidential-assets/tests/units/api/transfer.test.ts
+++ confidential-assets/tests/units/api/transfer.test.ts
@@ -6,20 +6,12 @@ import {
   sendAndWaitTx,
   TOKEN_ADDRESS,
 } from "../../helpers";
-import { preloadTables } from "../../helpers/wasmPollardKangaroo";
 
 describe("Transfer", () => {
   const alice = getTestAccount();
   const aliceConfidential = getTestConfidentialAccount(alice);
 
   const TRANSFER_AMOUNT = 2n;
-  it(
-    "Pre load wasm table map",
-    async () => {
-      await preloadTables();
-    },
-    longTestTimeout,
-  );
 
   it("should transfer money from Alice actual to pending balance", async () => {
     const recipientAccAddr = "0xbae983154b659e5d0e9cb7f84001fdedb06482125a8e2945f47c2bc6ccd00690";
@@ -27,14 +19,13 @@ describe("Transfer", () => {
     const transferTx = await confidentialAsset.transfer({
       senderDecryptionKey: aliceConfidential,
       amount: TRANSFER_AMOUNT,
-      sender: alice.accountAddress,
       tokenAddress: TOKEN_ADDRESS,
       recipient: recipientAccAddr,
+      signer: alice
     });
-    const txResp = await sendAndWaitTx(transferTx, alice);
 
-    console.log("gas used:", txResp.gas_used);
+    console.log("gas used:", transferTx.gas_used);
 
-    expect(txResp.success).toBeTruthy();
+    expect(transferTx.success).toBeTruthy();
   });
 });
