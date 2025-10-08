b75ce4d6ba649bb0dccf0d46d5f4fedc597a60b8
Andrew Hariri <31301117+hariria@users.noreply.github.com>
2025-10-07 17:17:28 -0700

    [ts-sdk] Add shelbynet endpoints (#771)
    * [ts-sdk] Add shelbynet endpoints
    
    * [sdk] Add changelog 5.1.1
    
    ---------
    
    Co-authored-by: Andrew Hariri <hariria@usc.edu>

--- src/utils/apiEndpoints.ts
+++ src/utils/apiEndpoints.ts
@@ -9,6 +9,7 @@ export const NetworkToIndexerAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/v1/graphql",
   testnet: "https://api.testnet.aptoslabs.com/v1/graphql",
   devnet: "https://api.devnet.aptoslabs.com/v1/graphql",
+  shelbynet: "https://api.shelbynet.shelby.xyz/v1/graphql",
   local: "http://127.0.0.1:8090/v1/graphql",
 };
 
@@ -20,6 +21,7 @@ export const NetworkToNodeAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/v1",
   testnet: "https://api.testnet.aptoslabs.com/v1",
   devnet: "https://api.devnet.aptoslabs.com/v1",
+  shelbynet: "https://api.shelbynet.shelby.xyz/v1",
   local: "http://127.0.0.1:8080/v1",
 };
 
@@ -29,6 +31,7 @@ export const NetworkToNodeAPI: Record<string, string> = {
  */
 export const NetworkToFaucetAPI: Record<string, string> = {
   devnet: "https://faucet.devnet.aptoslabs.com",
+  shelbynet: "https://faucet.shelbynet.shelby.xyz",
   local: "http://127.0.0.1:8081",
 };
 
@@ -40,6 +43,7 @@ export const NetworkToPepperAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/keyless/pepper/v0",
   testnet: "https://api.testnet.aptoslabs.com/keyless/pepper/v0",
   devnet: "https://api.devnet.aptoslabs.com/keyless/pepper/v0",
+  shelbynet: "https://api.devnet.aptoslabs.com/keyless/pepper/v0",
   // Use the devnet service for local environment
   local: "https://api.devnet.aptoslabs.com/keyless/pepper/v0",
 };
@@ -52,6 +56,7 @@ export const NetworkToProverAPI: Record<string, string> = {
   mainnet: "https://api.mainnet.aptoslabs.com/keyless/prover/v0",
   testnet: "https://api.testnet.aptoslabs.com/keyless/prover/v0",
   devnet: "https://api.devnet.aptoslabs.com/keyless/prover/v0",
+  shelbynet: "https://api.devnet.aptoslabs.com/keyless/prover/v0",
   // Use the devnet service for local environment
   local: "https://api.devnet.aptoslabs.com/keyless/prover/v0",
 };
@@ -65,6 +70,7 @@ export enum Network {
   MAINNET = "mainnet",
   TESTNET = "testnet",
   DEVNET = "devnet",
+  SHELBYNET = "shelbynet",
   LOCAL = "local",
   CUSTOM = "custom",
 }
@@ -87,6 +93,7 @@ export const NetworkToNetworkName: Record<string, Network> = {
   mainnet: Network.MAINNET,
   testnet: Network.TESTNET,
   devnet: Network.DEVNET,
+  shelbynet: Network.SHELBYNET,
   local: Network.LOCAL,
   custom: Network.CUSTOM,
 };
