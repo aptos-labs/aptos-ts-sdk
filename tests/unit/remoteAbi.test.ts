074e91255bd1bca36af4cb69df5ff7d7c263ee70
Greg Nazario <greg@gnazar.io>
2025-12-30 04:55:43 -0500

    [bcs] Fix signed integers in scripts, add tests

--- tests/unit/remoteAbi.test.ts
+++ tests/unit/remoteAbi.test.ts
@@ -15,6 +15,12 @@ import {
   U32,
   U64,
   U8,
+  I8,
+  I16,
+  I32,
+  I64,
+  I128,
+  I256,
 } from "../../src";
 
 const MAX_U8 = 255;
@@ -24,6 +30,20 @@ const MAX_U64 = 18_446_744_073_709_551_615n;
 const MAX_U128 = 340_282_366_920_938_463_463_374_607_431_768_211_455n;
 const MAX_U256 =
   115_792_089_237_316_195_423_570_985_008_687_907_853_269_984_665_640_564_039_457_584_007_913_129_639_935n;
+
+// Signed integer bounds
+const MIN_I8 = -128;
+const MAX_I8 = 127;
+const MIN_I16 = -32768;
+const MAX_I16 = 32767;
+const MIN_I32 = -2147483648;
+const MAX_I32 = 2147483647;
+const MIN_I64 = -9223372036854775808n;
+const MAX_I64 = 9223372036854775807n;
+const MIN_I128 = -170141183460469231731687303715884105728n;
+const MAX_I128 = 170141183460469231731687303715884105727n;
+const MIN_I256 = -57896044618658097711785492504343953926634992332820282019728792003956564819968n;
+const MAX_I256 = 57896044618658097711785492504343953926634992332820282019728792003956564819967n;
 
 describe("Remote ABI", () => {
   describe("Parse", () => {
@@ -52,6 +72,32 @@ describe("Remote ABI", () => {
       expect(checkOrConvertArgument(MAX_U256.toString(), parseTypeTag("u256"), 0, [])).toEqual(new U256(MAX_U256));
     });
 
+    it("should parse signed integer primitive arguments", () => {
+      // Small signed integers can be passed as numbers (positive and negative)
+      expect(checkOrConvertArgument(MAX_I8, parseTypeTag("i8"), 0, [])).toEqual(new I8(MAX_I8));
+      expect(checkOrConvertArgument(MIN_I8, parseTypeTag("i8"), 0, [])).toEqual(new I8(MIN_I8));
+      expect(checkOrConvertArgument(MAX_I16, parseTypeTag("i16"), 0, [])).toEqual(new I16(MAX_I16));
+      expect(checkOrConvertArgument(MIN_I16, parseTypeTag("i16"), 0, [])).toEqual(new I16(MIN_I16));
+      expect(checkOrConvertArgument(MAX_I32, parseTypeTag("i32"), 0, [])).toEqual(new I32(MAX_I32));
+      expect(checkOrConvertArgument(MIN_I32, parseTypeTag("i32"), 0, [])).toEqual(new I32(MIN_I32));
+
+      // Large signed integers can be passed as bigints and strings
+      expect(checkOrConvertArgument(MAX_I64, parseTypeTag("i64"), 0, [])).toEqual(new I64(MAX_I64));
+      expect(checkOrConvertArgument(MIN_I64, parseTypeTag("i64"), 0, [])).toEqual(new I64(MIN_I64));
+      expect(checkOrConvertArgument(MIN_I64.toString(), parseTypeTag("i64"), 0, [])).toEqual(new I64(MIN_I64));
+      expect(checkOrConvertArgument(MAX_I128, parseTypeTag("i128"), 0, [])).toEqual(new I128(MAX_I128));
+      expect(checkOrConvertArgument(MIN_I128, parseTypeTag("i128"), 0, [])).toEqual(new I128(MIN_I128));
+      expect(checkOrConvertArgument(MIN_I128.toString(), parseTypeTag("i128"), 0, [])).toEqual(new I128(MIN_I128));
+      expect(checkOrConvertArgument(MAX_I256, parseTypeTag("i256"), 0, [])).toEqual(new I256(MAX_I256));
+      expect(checkOrConvertArgument(MIN_I256, parseTypeTag("i256"), 0, [])).toEqual(new I256(MIN_I256));
+      expect(checkOrConvertArgument(MIN_I256.toString(), parseTypeTag("i256"), 0, [])).toEqual(new I256(MIN_I256));
+
+      // Small values as numbers for larger types
+      expect(checkOrConvertArgument(-5, parseTypeTag("i64"), 0, [])).toEqual(new I64(-5));
+      expect(checkOrConvertArgument(-5, parseTypeTag("i128"), 0, [])).toEqual(new I128(-5));
+      expect(checkOrConvertArgument(-5, parseTypeTag("i256"), 0, [])).toEqual(new I256(-5));
+    });
+
     // This test is for backward compatibility with the SDK V1 to generate transactions with loose types
     it("should parse primitive types for sdk v1 compatibility", () => {
       // boolean as a string
@@ -67,6 +113,17 @@ describe("Remote ABI", () => {
       expect(checkOrConvertArgument("255", parseTypeTag("u256"), 0, [])).toEqual(new U256(MAX_U8));
     });
 
+    it("should parse signed integers as strings", () => {
+      expect(checkOrConvertArgument("-128", parseTypeTag("i8"), 0, [])).toEqual(new I8(MIN_I8));
+      expect(checkOrConvertArgument("127", parseTypeTag("i8"), 0, [])).toEqual(new I8(MAX_I8));
+      expect(checkOrConvertArgument("-32768", parseTypeTag("i16"), 0, [])).toEqual(new I16(MIN_I16));
+      expect(checkOrConvertArgument("32767", parseTypeTag("i16"), 0, [])).toEqual(new I16(MAX_I16));
+      expect(checkOrConvertArgument("-2147483648", parseTypeTag("i32"), 0, [])).toEqual(new I32(MIN_I32));
+      expect(checkOrConvertArgument("2147483647", parseTypeTag("i32"), 0, [])).toEqual(new I32(MAX_I32));
+      expect(checkOrConvertArgument("-9223372036854775808", parseTypeTag("i64"), 0, [])).toEqual(new I64(MIN_I64));
+      expect(checkOrConvertArgument("9223372036854775807", parseTypeTag("i64"), 0, [])).toEqual(new I64(MAX_I64));
+    });
+
     it("should parse a typed arguments", () => {
       expect(checkOrConvertArgument(AccountAddress.ONE, parseTypeTag("address"), 0, [])).toEqual(AccountAddress.ONE);
       expect(checkOrConvertArgument(new Bool(true), parseTypeTag("bool"), 0, [])).toEqual(new Bool(true));
@@ -76,6 +133,21 @@ describe("Remote ABI", () => {
       expect(checkOrConvertArgument(new U64(MAX_U64), parseTypeTag("u64"), 0, [])).toEqual(new U64(MAX_U64));
       expect(checkOrConvertArgument(new U128(MAX_U128), parseTypeTag("u128"), 0, [])).toEqual(new U128(MAX_U128));
       expect(checkOrConvertArgument(new U256(MAX_U256), parseTypeTag("u256"), 0, [])).toEqual(new U256(MAX_U256));
+    });
+
+    it("should parse typed signed integer arguments", () => {
+      expect(checkOrConvertArgument(new I8(MIN_I8), parseTypeTag("i8"), 0, [])).toEqual(new I8(MIN_I8));
+      expect(checkOrConvertArgument(new I8(MAX_I8), parseTypeTag("i8"), 0, [])).toEqual(new I8(MAX_I8));
+      expect(checkOrConvertArgument(new I16(MIN_I16), parseTypeTag("i16"), 0, [])).toEqual(new I16(MIN_I16));
+      expect(checkOrConvertArgument(new I16(MAX_I16), parseTypeTag("i16"), 0, [])).toEqual(new I16(MAX_I16));
+      expect(checkOrConvertArgument(new I32(MIN_I32), parseTypeTag("i32"), 0, [])).toEqual(new I32(MIN_I32));
+      expect(checkOrConvertArgument(new I32(MAX_I32), parseTypeTag("i32"), 0, [])).toEqual(new I32(MAX_I32));
+      expect(checkOrConvertArgument(new I64(MIN_I64), parseTypeTag("i64"), 0, [])).toEqual(new I64(MIN_I64));
+      expect(checkOrConvertArgument(new I64(MAX_I64), parseTypeTag("i64"), 0, [])).toEqual(new I64(MAX_I64));
+      expect(checkOrConvertArgument(new I128(MIN_I128), parseTypeTag("i128"), 0, [])).toEqual(new I128(MIN_I128));
+      expect(checkOrConvertArgument(new I128(MAX_I128), parseTypeTag("i128"), 0, [])).toEqual(new I128(MAX_I128));
+      expect(checkOrConvertArgument(new I256(MIN_I256), parseTypeTag("i256"), 0, [])).toEqual(new I256(MIN_I256));
+      expect(checkOrConvertArgument(new I256(MAX_I256), parseTypeTag("i256"), 0, [])).toEqual(new I256(MAX_I256));
     });
 
     it("should parse a complex simple arguments", () => {
@@ -125,6 +197,14 @@ describe("Remote ABI", () => {
       );
       expect(checkOrConvertArgument(null, parseTypeTag("0x1::option::Option<u8>"), 0, [])).toEqual(
         new MoveOption<U8>(),
+      );
+
+      // Options with signed integers
+      expect(checkOrConvertArgument(-5, parseTypeTag("0x1::option::Option<i8>"), 0, [])).toEqual(
+        new MoveOption(new I8(-5)),
+      );
+      expect(checkOrConvertArgument(null, parseTypeTag("0x1::option::Option<i64>"), 0, [])).toEqual(
+        new MoveOption<I64>(),
       );
 
       // Objects are account addresses, and it doesn't matter about the type used
@@ -209,6 +289,36 @@ describe("Remote ABI", () => {
       );
     });
 
+    it("should parse vectors of signed integers", () => {
+      expect(checkOrConvertArgument([-128, 0, 127], parseTypeTag("vector<i8>"), 0, [])).toEqual(
+        MoveVector.I8([-128, 0, 127]),
+      );
+      expect(checkOrConvertArgument([-32768, 0, 32767], parseTypeTag("vector<i16>"), 0, [])).toEqual(
+        MoveVector.I16([-32768, 0, 32767]),
+      );
+      expect(checkOrConvertArgument([-2147483648, 0, 2147483647], parseTypeTag("vector<i32>"), 0, [])).toEqual(
+        MoveVector.I32([-2147483648, 0, 2147483647]),
+      );
+      expect(checkOrConvertArgument([-1000000n, 0n, 1000000n], parseTypeTag("vector<i64>"), 0, [])).toEqual(
+        MoveVector.I64([-1000000n, 0n, 1000000n]),
+      );
+      expect(checkOrConvertArgument([-1000000n, 0n, 1000000n], parseTypeTag("vector<i128>"), 0, [])).toEqual(
+        MoveVector.I128([-1000000n, 0n, 1000000n]),
+      );
+      expect(checkOrConvertArgument([-1000000n, 0n, 1000000n], parseTypeTag("vector<i256>"), 0, [])).toEqual(
+        MoveVector.I256([-1000000n, 0n, 1000000n]),
+      );
+    });
+
+    it("should parse vectors of signed integers from strings", () => {
+      expect(checkOrConvertArgument("[-128, 0, 127]", parseTypeTag("vector<i8>"), 0, [])).toEqual(
+        MoveVector.I8([-128, 0, 127]),
+      );
+      expect(checkOrConvertArgument("[-32768, 0, 32767]", parseTypeTag("vector<i16>"), 0, [])).toEqual(
+        MoveVector.I16([-32768, 0, 32767]),
+      );
+    });
+
     it("should fail on invalid simple inputs", () => {
       expect(() => checkOrConvertArgument(false, parseTypeTag("address"), 0, [])).toThrowError();
       expect(() => checkOrConvertArgument(0, parseTypeTag("bool"), 0, [])).toThrowError();
@@ -229,6 +339,19 @@ describe("Remote ABI", () => {
 
       // Unsupported type
       expect(() => checkOrConvertArgument(false, parseTypeTag("signer"), 0, [])).toThrowError();
+    });
+
+    it("should fail on invalid signed integer inputs", () => {
+      // Boolean inputs should fail for signed integers
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i8"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i16"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i32"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i64"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i128"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(false, parseTypeTag("i256"), 0, [])).toThrowError();
+
+      // Booleans should fail for vector<i*>
+      expect(() => checkOrConvertArgument(false, parseTypeTag("vector<i8>"), 0, [])).toThrowError();
     });
 
     it("should fail on typed inputs", () => {
@@ -255,6 +378,20 @@ describe("Remote ABI", () => {
 
       // Unsupported type
       expect(() => checkOrConvertArgument(false, parseTypeTag("signer"), 0, [])).toThrowError();
+    });
+
+    it("should fail on typed signed integer inputs with wrong type", () => {
+      // Bool inputs should fail for signed integers
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i8"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i16"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i32"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i64"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i128"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new Bool(true), parseTypeTag("i256"), 0, [])).toThrowError();
+
+      // Wrong signed integer type should fail
+      expect(() => checkOrConvertArgument(new I16(5), parseTypeTag("i8"), 0, [])).toThrowError();
+      expect(() => checkOrConvertArgument(new I8(5), parseTypeTag("i16"), 0, [])).toThrowError();
     });
 
     it("should not support unsupported vector conversions", () => {
--- tests/unit/signedIntegers.test.ts
+++ tests/unit/signedIntegers.test.ts
@@ -1,7 +1,7 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-import { Deserializer, Serializer, I8, I16, I32, I64, I128, I256 } from "../../src";
+import { Deserializer, Serializer, I8, I16, I32, I64, I128, I256, MoveVector, MoveOption } from "../../src";
 
 describe("Signed Integer Types", () => {
   describe("I8", () => {
@@ -183,6 +183,257 @@ describe("Signed Integer Types", () => {
       const deserializer = new Deserializer(bytes);
       const deserialized = I256.deserialize(deserializer);
       expect(deserialized.value).toBe(-57896044618658097711785492504343953926634992332820282019728792003956564819968n);
+    });
+  });
+
+  describe("MoveVector with Signed Integers", () => {
+    test("MoveVector.I8 creates and serializes correctly", () => {
+      const vec = MoveVector.I8([-128, -1, 0, 1, 127]);
+      expect(vec.values.length).toBe(5);
+      expect(vec.values[0].value).toBe(-128);
+      expect(vec.values[4].value).toBe(127);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I8);
+      expect(deserialized.values.map((v) => v.value)).toEqual([-128, -1, 0, 1, 127]);
+    });
+
+    test("MoveVector.I16 creates and serializes correctly", () => {
+      const vec = MoveVector.I16([-32768, -1, 0, 1, 32767]);
+      expect(vec.values.length).toBe(5);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I16);
+      expect(deserialized.values.map((v) => v.value)).toEqual([-32768, -1, 0, 1, 32767]);
+    });
+
+    test("MoveVector.I32 creates and serializes correctly", () => {
+      const vec = MoveVector.I32([-2147483648, -1, 0, 1, 2147483647]);
+      expect(vec.values.length).toBe(5);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I32);
+      expect(deserialized.values.map((v) => v.value)).toEqual([-2147483648, -1, 0, 1, 2147483647]);
+    });
+
+    test("MoveVector.I64 creates and serializes correctly", () => {
+      const vec = MoveVector.I64([-9223372036854775808n, -1n, 0n, 1n, 9223372036854775807n]);
+      expect(vec.values.length).toBe(5);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I64);
+      expect(deserialized.values.map((v) => v.value)).toEqual([
+        -9223372036854775808n,
+        -1n,
+        0n,
+        1n,
+        9223372036854775807n,
+      ]);
+    });
+
+    test("MoveVector.I128 creates and serializes correctly", () => {
+      const vec = MoveVector.I128([
+        -170141183460469231731687303715884105728n,
+        0n,
+        170141183460469231731687303715884105727n,
+      ]);
+      expect(vec.values.length).toBe(3);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I128);
+      expect(deserialized.values.map((v) => v.value)).toEqual([
+        -170141183460469231731687303715884105728n,
+        0n,
+        170141183460469231731687303715884105727n,
+      ]);
+    });
+
+    test("MoveVector.I256 creates and serializes correctly", () => {
+      const minI256 = -57896044618658097711785492504343953926634992332820282019728792003956564819968n;
+      const maxI256 = 57896044618658097711785492504343953926634992332820282019728792003956564819967n;
+      const vec = MoveVector.I256([minI256, 0n, maxI256]);
+      expect(vec.values.length).toBe(3);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I256);
+      expect(deserialized.values.map((v) => v.value)).toEqual([minI256, 0n, maxI256]);
+    });
+
+    test("empty MoveVector with signed integers", () => {
+      const vec = MoveVector.I8([]);
+      expect(vec.values.length).toBe(0);
+
+      const serializer = new Serializer();
+      vec.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveVector.deserialize(deserializer, I8);
+      expect(deserialized.values.length).toBe(0);
+    });
+  });
+
+  describe("MoveOption with Signed Integers", () => {
+    test("MoveOption.I8 with value", () => {
+      const opt = MoveOption.I8(-5);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(-5);
+
+      const serializer = new Serializer();
+      opt.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveOption.deserialize(deserializer, I8);
+      expect(deserialized.isSome()).toBe(true);
+      expect(deserialized.unwrap().value).toBe(-5);
+    });
+
+    test("MoveOption.I8 without value", () => {
+      const opt = MoveOption.I8();
+      expect(opt.isSome()).toBe(false);
+
+      const optNull = MoveOption.I8(null);
+      expect(optNull.isSome()).toBe(false);
+
+      const optUndefined = MoveOption.I8(undefined);
+      expect(optUndefined.isSome()).toBe(false);
+    });
+
+    test("MoveOption.I16 with value", () => {
+      const opt = MoveOption.I16(-1000);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(-1000);
+    });
+
+    test("MoveOption.I32 with value", () => {
+      const opt = MoveOption.I32(-100000);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(-100000);
+    });
+
+    test("MoveOption.I64 with value", () => {
+      const opt = MoveOption.I64(-9223372036854775808n);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(-9223372036854775808n);
+    });
+
+    test("MoveOption.I128 with value", () => {
+      const opt = MoveOption.I128(-170141183460469231731687303715884105728n);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(-170141183460469231731687303715884105728n);
+    });
+
+    test("MoveOption.I256 with value", () => {
+      const minI256 = -57896044618658097711785492504343953926634992332820282019728792003956564819968n;
+      const opt = MoveOption.I256(minI256);
+      expect(opt.isSome()).toBe(true);
+      expect(opt.unwrap().value).toBe(minI256);
+    });
+
+    test("MoveOption with signed integers serializes and deserializes correctly", () => {
+      const opt = MoveOption.I64(-12345n);
+
+      const serializer = new Serializer();
+      opt.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveOption.deserialize(deserializer, I64);
+      expect(deserialized.isSome()).toBe(true);
+      expect(deserialized.unwrap().value).toBe(-12345n);
+    });
+
+    test("Empty MoveOption with signed integers serializes and deserializes correctly", () => {
+      const opt = MoveOption.I32();
+
+      const serializer = new Serializer();
+      opt.serialize(serializer);
+      const bytes = serializer.toUint8Array();
+
+      const deserializer = new Deserializer(bytes);
+      const deserialized = MoveOption.deserialize(deserializer, I32);
+      expect(deserialized.isSome()).toBe(false);
+    });
+  });
+
+  describe("serializeForScriptFunction", () => {
+    test("I8 serializes for script function with correct variant", () => {
+      const value = new I8(-5);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 10 for I8
+      expect(bytes[0]).toBe(10);
+    });
+
+    test("I16 serializes for script function with correct variant", () => {
+      const value = new I16(-1000);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 11 for I16
+      expect(bytes[0]).toBe(11);
+    });
+
+    test("I32 serializes for script function with correct variant", () => {
+      const value = new I32(-100000);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 12 for I32
+      expect(bytes[0]).toBe(12);
+    });
+
+    test("I64 serializes for script function with correct variant", () => {
+      const value = new I64(-1000000n);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 13 for I64
+      expect(bytes[0]).toBe(13);
+    });
+
+    test("I128 serializes for script function with correct variant", () => {
+      const value = new I128(-1000000n);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 14 for I128
+      expect(bytes[0]).toBe(14);
+    });
+
+    test("I256 serializes for script function with correct variant", () => {
+      const value = new I256(-1000000n);
+      const serializer = new Serializer();
+      value.serializeForScriptFunction(serializer);
+      const bytes = serializer.toUint8Array();
+      // Variant 15 for I256
+      expect(bytes[0]).toBe(15);
     });
   });
 });
